27aedb3e2d642f6c4bc52e38f3745f46c0a7db58
BlastAsia Cloud02 <baicloud02@outlook.com>
2019-06-17 11:15:57 +0800

    Add project files.

---
+++ AbstractFactory/ConcreteFactories.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractFactory
+{
+	interface ISmartPhone
+	{
+		string GetModelDetails();
+	}
+	interface INormalPhone
+	{
+		string GetModelDetails();
+	}
+
+	class NokiaPixel : ISmartPhone
+	{
+		public string GetModelDetails()
+		{
+			return "Model: Nokia Pixel\nRAM: 3GB\nCamera: 8MP\n";
+		}
+	}
+
+	class SamsungGalaxy : ISmartPhone
+	{
+		public string GetModelDetails()
+		{
+			return "Model: Samsung Galaxy\nRAM: 2GB\nCamera: 13MP\n";
+		}
+	}
+
+	class Nokia1600 : INormalPhone
+	{
+		public string GetModelDetails()
+		{
+			return "Model: Nokia 1600\nRAM: NA\nCamera: NA\n";
+		}
+	}
+
+	class SamsungGuru : INormalPhone
+	{
+		public string GetModelDetails()
+		{
+			return "Model: Samsung Guru\nRAM: NA\nCamera: NA\n";
+		}
+	}
+}
---
+++ AbstractFactory/IMobileFactories.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractFactory
+{
+	interface IMobilePhone
+	{ 
+		ISmartPhone GetSmartPhone();
+		INormalPhone GetNormalPhone();
+	}
+
+	class Nokia : IMobilePhone
+	{
+		public ISmartPhone GetSmartPhone()
+		{
+			return new NokiaPixel();
+		}
+
+		public INormalPhone GetNormalPhone()
+		{
+			return new Nokia1600();
+		}
+	}
+
+	class Samsung : IMobilePhone
+	{
+		public ISmartPhone GetSmartPhone()
+		{
+			return new SamsungGalaxy();
+		}
+
+		public INormalPhone GetNormalPhone()
+		{
+			return new SamsungGuru();
+		}
+	}
+}
